730fad4e1f51b74abd850fcb7829ded07177f577
superbkibbles <mend_mzury@hotmail.com>
2020-11-07 15:50:45 +0300

    redirecting to another website

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,30 +2,41 @@ import React from 'react';
 import Logo from '../assets/pics/logoPic.png'
 const Test = () => {
     return (
-        <div>
+        <div style={{
+            height: '100vh',
+            display: "flex",
+            flexDirection: "column",
+        }}>
             <div style={{
                 backgroundColor: "black",
-                minHeight: "5vh",
+                // minHeight: "5%",
+                padding: '1.7rem',
                 width: "100vw",
                 display: "flex",
                 color: "var(--color-grey-light-1)",
                 fontSize: "2rem",
                 justifyContent: "space-around",
                 alignItems: "center",
-                flexWrap: "wrap"
+                flexWrap: "wrap",
+                marginBottom: "10rem"
             }}>
                 <div>
                     <p>Email: info@global-mw.com</p>
                 </div>
-                <a style={{
-                    color: "inherit"
-                }} href="tel:07504379494">Phone: 07504379494</a>
+                <p>
+                    Phone: <a style={{
+                    color: "inherit",
+                    borderBottom: "1px solid white",
+                    paddingBottom: ".2rem"
+                }} href="tel:07504379494">07504379494</a>
+
+                </p>
+
             </div>
             <div style={{
                 display: "flex",
                 justifyContent: "space-around",
                 alignItems: "center",
-                minHeight: "95vh"
             }}>
                 <div style={{
                     display: "flex",
