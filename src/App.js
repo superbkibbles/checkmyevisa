355062d274ce8ffafe4b6117fc119e4c6fa3635d
superbkibbles <mend_mzury@hotmail.com>
2020-11-07 15:07:21 +0300

    redirecting to another website

--- src/App.js
+++ src/App.js
@@ -15,46 +15,67 @@ function App() {
       //   <Route exact path='/some' component={() => <div>Well</div>} />
       // </Switch>
 
-      <div style={{
-          // backgroundColor: "#f5f5f5",
-
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          height: "100vh"
-      }}>
+      <div>
           <div style={{
+              backgroundColor: "black",
+              minHeight: "5vh",
+              width: "100vw",
               display: "flex",
+              color: "var(--color-grey-light-1)",
+              fontSize: "2rem",
               justifyContent: "space-around",
-              width: "100%",
+              alignItems: "center",
               flexWrap: "wrap"
           }}>
-              <img
-                  style={{
-                      width: "50%",
-                      height: "50%"
-                }}
-                  src={ pic }
+              <div>
+                  <p>Email: info@global-mw.com</p>
+              </div>
+              <a style={{
+                  color: "inherit"
+              }} href="tel:07504379494">Phone: 07504379494</a>
+          </div>
+          <div style={{
+              // backgroundColor: "#f5f5f5",
 
-               alt="Logo"/>
-
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+              minHeight: "95vh"
+          }}>
               <div style={{
-                  // backgroundColor: "yellow",
-                  width: "400px",
-                  height: "500px",
-                  // wordBreak: "break"
                   display: "flex",
-                  textAlign: "center",
-                  alignItems: "center",
-
+                  justifyContent: "space-around",
+                  width: "100%",
+                  flexWrap: "wrap"
               }}>
-                  <h1 style={{
-                      fontSize: "7rem",
+                  <img
+                      style={{
+                          width: "50%",
+                          height: "50%"
+                      }}
+                      src={ pic }
+
+                      alt="Logo"/>
+
+                  <div style={{
+                      // backgroundColor: "yellow",
+                      width: "400px",
+                      height: "500px",
+                      // wordBreak: "break"
+                      display: "flex",
+                      textAlign: "center",
+                      alignItems: "center",
+
                   }}>
+                      <h1 style={{
+                          fontSize: "5rem",
+                          boxDecorationBreak: "clone"
+                      }}>
                       <span style={{
                           color: "#ec9338"
                       }}>Website</span> is in development
-                  </h1>
+                      </h1>
+                  </div>
               </div>
           </div>
       </div>
